918df8bcfec5cf3ae4b35b7cbc9aa54b204676e7
Sandeep M <723msfree@gmail.com>
2025-08-28 12:22:31 +0000

    ./node_modules/handlebars/lib/index.js require.extensions is not support

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
-import AIStyleAdvisor from './AIStyleAdvisor';
+import Link from 'next/link';
 
 export default function Hero() {
   return (
     <div className="relative rounded-lg overflow-hidden min-h-[400px] md:min-h-[500px] flex items-center">
       <Image
-        src="https://picsum.photos/id/21/1440/500"
+        src="https://picsum.photos/id/1025/1440/500"
         alt="Stylish person wearing glasses"
         fill
         className="object-cover"
@@ -22,10 +22,9 @@ export default function Hero() {
           Discover eyeglasses that perfectly match your style and vision needs. Try them on virtually from the comfort of your home.
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <Button size="lg" className="bg-primary hover:bg-primary/90 text-primary-foreground">
-            Shop Collection
+          <Button size="lg" asChild>
+            <Link href="/men">Shop Collection</Link>
           </Button>
-          <AIStyleAdvisor />
         </div>
       </div>
     </div>
