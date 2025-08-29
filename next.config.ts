919d9966f700faf82f1522f139b1fa07fea4044a
Sandeep M <723msfree@gmail.com>
2025-08-29 03:22:25 +0000

    Add Subdirectory Support Once basic deployment works, add back to next.c

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/optivision',
+  assetPrefix: '/optivision/',
   images: {
     unoptimized: true,
   },
