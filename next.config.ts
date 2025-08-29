cd3f7a2555af6864e29f8860ec255487dcb95df3
Sandeep M <723msfree@gmail.com>
2025-08-29 04:44:24 +0000

    Asset Paths & Caching
    All assets (/_next/…) are correctly prefixed with

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/optivision',
-  assetPrefix: '/optivision/',
+  assetPrefix: `/optivision/v${process.env.npm_package_version}/`,
   trailingSlash: true,
   images: {
     unoptimized: true,
