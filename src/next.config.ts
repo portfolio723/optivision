0f6160ac237bc0b4a81110301b66ad9bc3197ceb
Sandeep M <723msfree@gmail.com>
2025-08-29 04:03:02 +0000

    Missing index.html for Dynamic Routes Ensure that for every dynamic rout

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/optivision',
+  assetPrefix: '/optivision/',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
